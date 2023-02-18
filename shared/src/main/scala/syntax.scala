13f54cc4609a56a1afdc148214e0081c50f8e477
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-02-19 00:31:45 +0100

    Add update utilities

--- shared/src/main/scala/syntax.scala
+++ shared/src/main/scala/syntax.scala
@@ -14,6 +14,23 @@ extension [A, R](inline simple: Ascend[R, A, Unit])
   inline def adaptNow(src: Source[A, Unit]): R = simple.adapt(src).value
 
 
+extension [A](inline place: Source[A, Unit] & Sink[A, Unit])
+  inline def update(inline f: A => A): Unit =
+    place.set(f(place.value))
+  inline def updateOptional(inline f: A => Option[A]): Unit = f(place.value) match
+    case Some(rv) => place.set(rv)
+    case None => ()
+
+extension [A](inline place: Source[Option[A], Unit] & Sink[A, Unit])
+  inline def updatePresent(inline f: A => A): Unit = place.value match
+    case Some(v) => place.set(f(v))
+    case None => ()
+  inline def updatePresentOptional(inline f: A => Option[A]): Unit = place.value match
+    case Some(v) => f(v) match
+      case Some(rv) => place.set(rv)
+      case None => ()
+    case None => ()
+
 //extension [X, R](sadapt: Producer[X, R])
 //  transparent inline def -->(s: Sink[X, R]): R =
 //    sadapt(s)
